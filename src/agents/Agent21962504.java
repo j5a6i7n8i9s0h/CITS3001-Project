8f7201e6ef87b494830e8c3dde65e5dda2ad058d
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2018-10-04 21:45:58 +0800

    Agent in progress

---
+++ src/agents/Agent21962504.java
@@ -0,0 +1,36 @@
+package agents;
+import hanabAI.*;
+
+
+public class Agent21962504 implements Agent {
+	
+	private Colour[] colours;
+	private int[] values;
+	
+	@Override
+	public Action doAction(State s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/*
+	 * 
+	 * https://int8.io/monte-carlo-tree-search-beginners-guide/
+	 * Potential actions 
+	 	* discard last 
+	 	* hint colour 
+	 	* hint value 
+	 	* Hint usefulness (keep or play) 
+	 	* hint uselessenss (discard) 
+	 	* Play If certain
+	 		* 	 Base off hints 
+	 	* 
+	 * 
+	 * 
+	 * */
+	
+	
+	public String toString() {
+		return "plzwork";
+	}
+	
+}
